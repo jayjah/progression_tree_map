03ff9b3de39b317c5ce1b08af09c1535af5e30e7
Markus jayjah <markus@jayjah.de>
2024-07-28 16:27:34 +0200

    add: idditonal fields to treenode, bump version

--- lib/src/classes/tree_node.dart
+++ lib/src/classes/tree_node.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 
 /// This is the individual node that forms the tree
 class TreeNode {
+  final String? name;
+  final String? content;
+  final Widget? icon;
+
   /// The [Widget] the node contains
   final Widget? child;
 
@@ -34,43 +38,55 @@ class TreeNode {
   /// The popup widget to show when the node is tapped
   final Widget? popUpWidget;
 
-  const TreeNode(
-      {this.child,
-      this.nodes = const [],
-      this.offset = Offset.zero,
-      this.angle = 0,
-      this.depth = 0,
-      this.decoration,
-      this.partnerWidget,
-      this.partnerWidgetOffset,
-      this.size,
-      this.firstChildPos,
-      this.popUpWidget});
+  const TreeNode({
+    this.child,
+    this.nodes = const [],
+    this.offset = Offset.zero,
+    this.angle = 0,
+    this.depth = 0,
+    this.decoration,
+    this.partnerWidget,
+    this.partnerWidgetOffset,
+    this.size,
+    this.firstChildPos,
+    this.popUpWidget,
+    this.name,
+    this.content,
+    this.icon,
+  });
 
-  TreeNode copyWith(
-      {Widget? child,
-      List<TreeNode>? nodes,
-      Offset? offset,
-      String? id,
-      int? depth,
-      double? size,
-      double? angle,
-      Widget? partnerWidget,
-      Offset? partnerWidgetOffset,
-      BoxDecoration? decoration,
-      int? firstChildPos,
-      Widget? popUpWidget}) {
+  TreeNode copyWith({
+    Widget? child,
+    List<TreeNode>? nodes,
+    Offset? offset,
+    String? id,
+    int? depth,
+    double? size,
+    double? angle,
+    Widget? partnerWidget,
+    Offset? partnerWidgetOffset,
+    BoxDecoration? decoration,
+    int? firstChildPos,
+    Widget? popUpWidget,
+    Widget? icon,
+    String? name,
+    String? content,
+  }) {
     return TreeNode(
-        child: child ?? this.child,
-        nodes: nodes ?? this.nodes,
-        depth: depth ?? this.depth,
-        offset: offset ?? this.offset,
-        size: size ?? this.size,
-        partnerWidget: partnerWidget ?? this.partnerWidget,
-        partnerWidgetOffset: partnerWidgetOffset ?? this.partnerWidgetOffset,
-        angle: angle ?? this.angle,
-        decoration: decoration ?? this.decoration,
-        firstChildPos: firstChildPos ?? this.firstChildPos,
-        popUpWidget: popUpWidget ?? this.popUpWidget);
+      child: child ?? this.child,
+      nodes: nodes ?? this.nodes,
+      depth: depth ?? this.depth,
+      offset: offset ?? this.offset,
+      size: size ?? this.size,
+      partnerWidget: partnerWidget ?? this.partnerWidget,
+      partnerWidgetOffset: partnerWidgetOffset ?? this.partnerWidgetOffset,
+      angle: angle ?? this.angle,
+      decoration: decoration ?? this.decoration,
+      firstChildPos: firstChildPos ?? this.firstChildPos,
+      popUpWidget: popUpWidget ?? this.popUpWidget,
+      name: name ?? this.name,
+      content: content ?? this.content,
+      icon: icon ?? this.icon,
+    );
   }
 }
